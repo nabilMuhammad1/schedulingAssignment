4a2d2b8079f03ea1aaf936a3890cf84d201cabbc
nabilMuhammad1 <44117070+nabilMuhammad1@users.noreply.github.com>
2018-12-03 10:27:14 -0800

    Update fcfs.py

--- fcfs.py
+++ fcfs.py
@@ -1,13 +1,13 @@
 print('First come Fisrt serve')
 arr_t = [0, 2, 4]
-burst_t = [4, 3, 5,6]
-wait_t = [0]*5
+burst_t = [4, 3, 5]
+wait_t = [0]*3
 start_t = 0
 finish_t = 0
-turnaround_t= [0]*5
+turnaround_t= [0]*3
 avg=0.0
 
-for index in range(5):
+for index in range(3):
 	print 'Process', index
 	finish_t = finish_t + burst_t[index]
 	wait_t[index] = start_t - arr_t[index]
@@ -17,7 +17,7 @@ for index in range(5):
 	print'Turnaround Time:', turnaround_time[index]
 	start_t= start_t+burst_t[index]
 print 'Average Turnaround Time'
-for k in range(5):
+for k in range(3):
 	avg=avg+turnaround_time[k]
-avg=avg/5
+avg=avg/3
 print(avg)
